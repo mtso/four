cfb57e1513a53830ff944db7220e785b8e55e966
mtso <mtso@tuta.io>
2017-02-19 00:43:37 -0800

    Add search functionality to read.js

--- read.js
+++ read.js
@@ -1,9 +1,14 @@
 var Datastore = require('nedb');
 
-var db = new Datastore({ filename: './data/definitions.db', autoload: true });
+var db = new Datastore({ filename: './data/definitionsA.db', autoload: true });
+var search = process.argv[2];
 
-db.find({ word: 'word' }, function (err, docs) {
+db.find({ word: search }, function (err, docs) {
+  if (err) throw err;
   docs.forEach(function(doc) {
-    console.log(JSON.stringify(doc));
+    console.log(doc.word);
+    doc.definitions.forEach(function(def, i) {
+      console.log( (i + 1) + ': ' + def.definition);
+    });
   });
 });
