87b4f608e2f2c628f39e9ce3ff41668478a949db
mtso <mtso@tuta.io>
2017-02-19 22:10:34 -0800

    Add localstorage dependency

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-var define = require('../define.js');
+var define = require('./define.js');
+var path = require('path');
+
+if (typeof localStorage === 'undefined' || localStorage === null) {
+  var LocalStorage = require('node-localstorage').LocalStorage;
+  var localStoragePath = path.join(__dirname, '../data/word-localstore');
+  localStorage = new LocalStorage(localStoragePath);
+}
+
+localStorage.setItem('myFirstKey', 'myFirstValue');
+console.log(localStorage.getItem('myFirstKey'));
