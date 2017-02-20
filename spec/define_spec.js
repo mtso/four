488b9ae82987c050d33495af1adea0daa993eea2
mtso <mtso@tuta.io>
2017-02-19 21:46:22 -0800

    Add search func

--- spec/define_spec.js
+++ spec/define_spec.js
@@ -1,16 +1,29 @@
-var defineA = require('../src/define.js');
+var w = require('../src/define.js');
 
 describe('define', function() {
+
   it('returns the definition of a word', function() {
     var expected = [
       "A small restaurant selling light meals and drinks.",
       "A bar or nightclub.",
       "A shop selling sweets, cigarettes, newspapers, etc. and staying open after normal hours."
     ];
-    expect(defineA('cafe')).toEqual(expected);
-    // var expectedDefinition = 'a unit of language';
-    // define('word', function(definition) {
-    //   expect(definition).toBe(expectedDefinition);
-    // });
+    expect(w.define('cafe')).toEqual(expected);
+  });
+
+  it('searches the word list', function() {
+    var expected = [
+      'cafe'
+    ];
+    expect(w.search('c')).toEqual(expected);
+    expect(w.search('caf')).toEqual(expected);
+
+    expected = [
+      "aahs",
+      "aals",
+      "abac"
+    ];
+    expect(w.search('a')).toEqual(expected);
+    expect(w.search('aa')).toEqual(expected.slice(0, 2));
   });
 });
--- src/define.js
+++ src/define.js
@@ -5,10 +5,25 @@ function define(word) {
   return data.definitions[word];
 }
 
-module.exports = function(word, callback) {
-  return define(word);
-  // var deferred = Q.defer();
-  // return deferred.promise;
-  // var definition = '';
-  // return callback(definition);
+function words() {
+  return data.words;
 }
+
+function search(text) {
+  return data.words.filter(function(word) {
+    return text === word.substring(0, text.length);
+  });
+}
+
+module.exports = {
+  define: define,
+  search: search
+}
+
+// function(word, callback) {
+//   return define(word);
+//   // var deferred = Q.defer();
+//   // return deferred.promise;
+//   // var definition = '';
+//   // return callback(definition);
+// }
