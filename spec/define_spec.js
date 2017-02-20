f7cddf57c254d02554394e939bba0175e0a3ccd6
mtso <mtso@tuta.io>
2017-02-19 21:39:50 -0800

    add define func

--- spec/define_spec.js
+++ spec/define_spec.js
@@ -1,10 +1,16 @@
-var define = require('../define.js');
+var defineA = require('../src/define.js');
 
 describe('define', function() {
   it('returns the definition of a word', function() {
-    var expectedDefinition = 'a unit of language';
-    define('word', function(definition) {
-      expect(definition).toBe(expectedDefinition);
-    });
+    var expected = [
+      "A small restaurant selling light meals and drinks.",
+      "A bar or nightclub.",
+      "A shop selling sweets, cigarettes, newspapers, etc. and staying open after normal hours."
+    ];
+    expect(defineA('cafe')).toEqual(expected);
+    // var expectedDefinition = 'a unit of language';
+    // define('word', function(definition) {
+    //   expect(definition).toBe(expectedDefinition);
+    // });
   });
 });
--- src/define.js
+++ src/define.js
@@ -1,10 +1,14 @@
 var Q = require('q');
+var data = require('./data.js');
+
+function define(word) {
+  return data.definitions[word];
+}
 
 module.exports = function(word, callback) {
-  var deferred = Q.defer();
-  
-  return deferred.promise;
-
-  var definition = '';
-  return callback(definition);
+  return define(word);
+  // var deferred = Q.defer();
+  // return deferred.promise;
+  // var definition = '';
+  // return callback(definition);
 }
