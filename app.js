842f4f84518c92be6f4e381012657f2feb3e226b
mtso <mtso@tuta.io>
2017-02-25 16:28:21 -0800

    Make local app easier to start

--- app.js
+++ app.js
@@ -3,9 +3,14 @@
 var express = require('express');
 
 var app = express();
+
 app.use('/four', express.static('docs'));
 
-var port = process.env.PORT || 1337;
+app.use('/*', function(req, res) {
+  res.redirect('/four');
+});
+
+var port = process.env.PORT || 4444;
 app.listen(port, function() {
   console.log('Serving to localhost:' + port + '/four');
 });
